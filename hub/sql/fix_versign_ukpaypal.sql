47ed1ff9dcefa247425889bb08628f5d625c4330
imapollo <ze.apollo@gmail.com>
2013-02-18 03:18:30 +0000

    Add fix_verisign_ukpaypal script.

---
+++ hub/sql/fix_versign_ukpaypal.sql
@@ -0,0 +1,5 @@
+update verisign_constants set HOST_ADDRESS = 'paypal.mockservice.stubcorp.dev' where ID = 322 and PARTNER = 'PayPalUK';
+update verisign_constants set HOST_ADDRESS = 'paypal.mockservice.stubcorp.dev' where ID = 323 and PARTNER = 'PayPalUK';
+update verisign_constants set HOST_ADDRESS = 'paypal.mockservice.stubcorp.dev' where ID = 3 and PARTNER = 'verisign';
+update verisign_constants set HOST_ADDRESS = 'paypal.mockservice.stubcorp.dev' where ID = 223 and PARTNER = 'verisign';
+commit;
