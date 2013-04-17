d7f4e07e31342c806f81a3b5de47deac425bd4c1
imapollo <ze.apollo@gmail.com>
2013-04-17 08:28:09 +0000

    #7 Get the branch name based on envid.

---
+++ util/ModuleTemplate.pm
@@ -0,0 +1,36 @@
+package Stubhub::ENV::Info;
+
+#
+# TODO baseic inforamtion about the module.
+#
+
+use strict;
+use warnings;
+
+use lib '/nas/reg/lib/perl';
+use lib '/nas/home/minjzhang/ops/util/lib'; # TODO Determine if this should be removed
+
+use Readonly;
+
+BEGIN {
+  use Exporter();
+  our ( $VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS );
+
+  $VERSION = do { my @r = (q$Revision: 1.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+  @ISA          = qw( Exporter );
+  @EXPORT       = qw();
+  # TODO rename the function
+  @EXPORT_OK    = qw(
+                        &sample_function
+                    );
+  %EXPORT_TAGS  = ();
+}
+
+our @EXPORT_OK;
+
+#
+# TODO rename the function
+#
+sub sample_function {
+}
---
+++ util/lib/Stubhub/ENV/Info.pm
@@ -0,0 +1,46 @@
+package Stubhub::ENV::Info;
+
+#
+# Get basic Stubhub Dev environment information.
+#
+
+use strict;
+use warnings;
+
+use lib '/nas/reg/lib/perl';
+use lib '/nas/home/minjzhang/ops/util/lib';
+
+use Readonly;
+use Stubhub::Util::Command qw (
+                                ssh_cmd
+                            );
+
+BEGIN {
+  use Exporter();
+  our ( $VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS );
+
+  $VERSION = do { my @r = (q$Revision: 1.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+
+  @ISA          = qw( Exporter );
+  @EXPORT       = qw();
+  @EXPORT_OK    = qw(
+                        &get_env_branch
+                    );
+  %EXPORT_TAGS  = ();
+}
+
+our @EXPORT_OK;
+
+#
+# Get the branch name of the environment.
+#
+sub get_env_branch {
+    my ( $envid ) = @_;
+    Readonly my $JOB_SERVER => "${envid}job001.${envid}.com";
+    Readonly my $STUBHUB_EAR_PATH => '/opt/jboss/server/job/deploy/stubhub';
+    my $command = "ls -l $STUBHUB_EAR_PATH | awk '{ print \$11 }' | awk -F/ '{ print \$4 }' | awk -F- '{ print \$1 }'";
+    my ( $status, @output ) = ssh_cmd( $JOB_SERVER, $command );
+    my $branch_name = join "", @output;
+    chomp $branch_name;
+    return $branch_name;
+}
