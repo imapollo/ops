522825ef10c7004930e9e664415bd3e1f06c4d71
imapollo <ze.apollo@gmail.com>
2013-05-07 08:32:45 +0000

    Modify perl module template.

--- util/ModuleTemplate.pm
+++ util/ModuleTemplate.pm
@@ -16,7 +16,7 @@ BEGIN {
   use Exporter();
   our ( $VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS );
 
-  $VERSION = do { my @r = (q$Revision: 1.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+  $VERSION = do { my @r = (q$Revision: 1.0 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
 
   @ISA          = qw( Exporter );
   @EXPORT       = qw();
