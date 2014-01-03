b1ca267fc52a84d7f9437fec91a9469e21a48e28
Minjie Zhang <ze.apollo@gmail.com>
2014-01-03 02:59:42 +0000

    Trivial fix for instagram like.

--- pilot/instagram_like.py
+++ pilot/instagram_like.py
@@ -11,20 +11,19 @@ from instagram.client import InstagramAPI
 # Client for instagram REST API.
 class InstagramClient():
 
-    def __init__():
+    def __init__( self ):
         access_token = "CHANGEME"
-        client = InstagramAPI( access_token = access_token )
-        return client
+        self.api = InstagramAPI( access_token = access_token )
 
 # Main.
 def main():
     client = InstagramClient()
-    searched_medias = client.tag_search( "vsco" , 100 )*
+    searched_medias = client.api.tag_search( "vsco" , 10 )
     for searched_media in searched_medias:
         # TODO sleep 2 seconds
         # TODO install python-instagram
         # client.media_likes( searched_media )
-        client.like_media( searched_media )
+        client.api.like_media( searched_media )
 
 if __name__ == "__main__":
     main()
