7ccd799fc941ed5b02dc065b5534d7d5cd5fe0e6
Minjie Zhang <ze.apollo@gmail.com>
2014-01-03 04:24:26 +0000

    Finish the instagram likes.

--- pilot/instagram_like.py
+++ pilot/instagram_like.py
@@ -7,6 +7,7 @@
 #
 
 from instagram.client import InstagramAPI
+import commands
 
 # Client for instagram REST API.
 class InstagramClient():
@@ -18,12 +19,13 @@ class InstagramClient():
 # Main.
 def main():
     client = InstagramClient()
-    searched_medias = client.api.tag_search( "vsco" , 10 )
-    for searched_media in searched_medias:
-        # TODO sleep 2 seconds
-        # TODO install python-instagram
-        # client.media_likes( searched_media )
-        client.api.like_media( searched_media )
+    searched_medias = client.api.tag_recent_media( 50, 999999999, "vsco" )
+    for searched_media in searched_medias[0]:
+        commands.getoutput( '/bin/sleep 2' )
+        likes = client.api.media_likes( searched_media.id )
+        if ( len(likes) > 0 ):
+            print searched_media.id
+            client.api.like_media( searched_media.id )
 
 if __name__ == "__main__":
     main()
