b93186e43178c4b08bc8f961005612ed3a7ba4a3
Minjie Zhang <ze.apollo@gmail.com>
2014-01-03 07:31:53 +0000

    Change like behavior.

--- pilot/instagram_like.py
+++ pilot/instagram_like.py
@@ -20,13 +20,13 @@ class InstagramClient():
 # Main.
 def main():
     client = InstagramClient()
-    searched_medias = client.api.tag_recent_media( 50, 999999999, "vsco" )
+    searched_medias = client.api.tag_recent_media( 20, 999999999, "vsco" )
     for searched_media in searched_medias[0]:
-        commands.getoutput( '/bin/sleep 2' )
-        likes = client.api.media_likes( searched_media.id )
-        if ( len(likes) > 4 ):
-            print ("%s: %s" % ( datetime.datetime.now(), searched_media.id ) )
-            client.api.like_media( searched_media.id )
+        commands.getoutput( '/bin/sleep 10' )
+        # likes = client.api.media_likes( searched_media.id )
+        # if ( len(likes) > 1 ):
+        print ("%s: %s" % ( datetime.datetime.now(), searched_media.id ) )
+        client.api.like_media( searched_media.id )
 
 if __name__ == "__main__":
     main()
