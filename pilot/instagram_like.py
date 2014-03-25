311d66b066cc067c0732c7a2f7476326e1696368
Minjie Zhang <ze.apollo@gmail.com>
2014-03-25 07:00:37 +0000

    Update instagram like script.

--- pilot/instagram_like.py
+++ pilot/instagram_like.py
@@ -14,19 +14,22 @@ import datetime
 class InstagramClient():
 
     def __init__( self ):
-        access_token = "CHANGEME"
+        access_token = "529078.1fb234f.ce9c7ff887c64add9b02b139253e64e0"
         self.api = InstagramAPI( access_token = access_token )
 
 # Main.
 def main():
     client = InstagramClient()
-    searched_medias = client.api.tag_recent_media( 20, 999999999, "vsco" )
+    searched_medias = client.api.tag_recent_media( 30, 999999999, "vsco" )
     for searched_media in searched_medias[0]:
-        commands.getoutput( '/bin/sleep 10' )
-        # likes = client.api.media_likes( searched_media.id )
-        # if ( len(likes) > 1 ):
-        print ("%s: %s" % ( datetime.datetime.now(), searched_media.id ) )
-        client.api.like_media( searched_media.id )
+        likes = client.api.media_likes( searched_media.id )
+        if ( len(likes) > 2 ):
+            commands.getoutput( '/bin/sleep 30' )
+            print ("%s: %s" % ( datetime.datetime.now(), searched_media.id ) )
+            try:
+                client.api.like_media( searched_media.id )
+            except Exception:
+                pass
 
 if __name__ == "__main__":
     main()
