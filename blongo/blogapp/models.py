dd28124230297bf7a7cb4af98b0a409aac8a0a24
Minjie Zhang <ze.apollo@gmail.com>
2014-03-25 06:58:16 +0000

    Initate blongo django project.

---
+++ blongo/blogapp/models.py
@@ -0,0 +1,10 @@
+from mongoengine import *
+from blongo.settings import DBNAME
+
+connect(DBNAME)
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    content = StringField(max_length=500, required=True)
+    last_update = DateTimeField(required=True)
+
---
+++ blongo/blongo/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'blongo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blogapp.views.index'),
+    url(r'^update/', 'blogapp.views.update'),
+    url(r'^delete/', 'blogapp.views.delete'),
+)
