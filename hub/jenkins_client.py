f093fd256614c3ed1953807c95c3f223806253c5
imapollo <ze.apollo@gmail.com>
2014-04-17 03:45:42 +0000

    #72 Enhance the jenkins client.

--- hub/jenkins_client.py
+++ hub/jenkins_client.py
@@ -7,21 +7,44 @@
 from jenkinsapi import jenkins
 import time
 
-jclient = jenkins.Jenkins("http://int.testing.stubcorp.dev/jenkins",
-            username="minjzhang", password="chinSk18w")
+#
+# Jenkins Client
+#
+class JenkinsClient:
 
-job_name = "com.stubhub.devops.smoketest.3"
-#print jclient.get_queue_url()
-#print jclient.base_server_url()
-build_params = { 'pool': 'srwd83', 'site': 'UK' }
-next_build_number = jclient[ job_name ].get_next_build_number()
-jclient.build_job( job_name, params=build_params )
-time.sleep( 10 )
-build_ids = jclient[ job_name ].get_build_ids()
+    # Initiate the client.
+    def __init__( self, jenkins_url ):
+        self.jenkins_username = 'minjzhang'
+        jenkins_password = 'chinSk18w'
+        self.jclient = jenkins.Jenkins( jenkins_url,
+                username=self.jenkins_username, password=jenkins_password)
 
-for build_id in build_ids:
-    if ( build_id >= next_build_number ):
-        build = jclient[ job_name ].get_build( build_id )
-        print build.get_result_url()
-    else:
-        break
+    # Trigger build for a job with optional parameters.
+    def build_job( self, job_name, build_params ):
+        next_build_number = self.jclient[ job_name ].get_next_build_number()
+        self.jclient.build_job( job_name, params=build_params )
+        time.sleep( 10 )
+        build_ids = self.jclient[ job_name ].get_build_ids()
+
+        for build_id in build_ids:
+            if ( build_id >= next_build_number ):
+                build = self.jclient[ job_name ].get_build( build_id )
+                build_actions = build.get_actions()
+                build_parameters = build_actions[ 'parameters' ]
+                build_user = build_actions[ 'causes' ][0][ 'userId' ]
+                if ( build_user == self.jenkins_username ):
+                    return build_id
+            else:
+                break
+
+        return 0
+
+# Main.
+def main():
+    jclient = JenkinsClient( 'http://int.testing.stubcorp.dev/jenkins' )
+    build_params = { 'pool': 'srwd83', 'site': 'UK' }
+    build_number = jclient.build_job( "com.stubhub.devops.smoketest", build_params )
+    print build_number
+
+if __name__ == "__main__":
+    main()
