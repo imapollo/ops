bb82535c08af867f084f78c46e6535fefa5e040b
Minjie Zhang <ze.apollo@gmail.com>
2016-11-24 18:34:20 +0800

    add option --column

--- tool/datamapping.py
+++ tool/datamapping.py
@@ -53,6 +53,7 @@ def main(argv=None):  # IGNORE:C0111
             formatter_class=RawDescriptionHelpFormatter)
         parser.add_argument("-d", "--data", dest="data_file", default=None, help="data file path")
         parser.add_argument("-m", "--mapping", dest="mapping_file", default=None, help="mapping file path")
+        parser.add_argument("-c", "--column", dest="column", default=1, help="column number")
         parser.add_argument('-D', '--debug', dest="debug", action='store_true', help="turn on DEBUG switch")
         parser.add_argument('-v', '--version', action='version', help="print the version ",
             version=program_version_message)
@@ -77,18 +78,42 @@ def main(argv=None):  # IGNORE:C0111
         sys.stderr.write(indent + "  for help use --help\n")
         return 2
 
+    if (args.column == 1):
+        pass
+    else:
+        column_pattern= "[^\\t]+\\t"
+        pattern = ""
+        i = 1
+        while i < int(args.column):
+            pattern += column_pattern
+            i += 1
+        pattern = "(%s)(%s)(.*)" % (pattern, "[0-9]+")
+
     id_map = get_id_map(args.mapping_file)
     data_file = open(args.data_file, 'r')
     for line in data_file:
-        match = re.match( r'([0-9]+).*', line)
-        if match:
-            old_id = match.group(1)
-            if (old_id in id_map):
-                print(line.replace(old_id, id_map[old_id], 1), end='')
+        if (args.column == 1):
+            match = re.match( r'([0-9]+).*', line)
+            if match:
+                old_id = match.group(1)
+                if (old_id in id_map):
+                    print(line.replace(old_id, id_map[old_id], 1), end='')
+                else:
+                    print("ERROR: No id [%s] in the map" % old_id)
             else:
-                print("ERROR: No id [%s] in the map" % old_id)
+                print("ERROR: No match on line: %s" % line)
         else:
-            print("ERROR: No match on line: %s" % line)
+            columns_match = re.match(r"%s" % pattern, line)
+            if (columns_match):
+                old_id = columns_match.group(2)
+                if (old_id in id_map):
+                    first_part = columns_match.group(1)
+                    third_part = columns_match.group(3)
+                    print(first_part + id_map[old_id] + third_part)
+                else:
+                    print("ERROR: No id [%s] in the map" % old_id)
+            else:
+                print("ERROR: No match on line: %s" % line)
 
 if __name__ == "__main__":
     main()
